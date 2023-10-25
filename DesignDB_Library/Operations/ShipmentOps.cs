72142bb12d98f9fd59a549acd759f4766fc8dc96
Roy Harbert <rharbert981@comcast.net>
2023-10-25 11:04:06 -0400

    all hyperlinks done

--- DesignDB_Library/Operations/ShipmentOps.cs
+++ DesignDB_Library/Operations/ShipmentOps.cs
@@ -17,6 +17,7 @@ using System.Configuration;
 using System.Drawing;
 using System.Windows.Forms;
 using NuGet;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace DesignDB_Library.Operations
 {
@@ -741,6 +742,7 @@ namespace DesignDB_Library.Operations
         {
             (Excel.Workbook wkb, int row, string wksName) rtn;
             Excel.Worksheet wks = wkbResults.ActiveSheet;
+            wks.Hyperlinks.Add(wks.Cells[1, 1], "", "Summary!A1", "Summary Page","Go to Summary Page");
             int row = MakeHeader(wks, startRow, 15, "BOM Lines with Matches in Shipments");
             InsertText(wks, row, 1, "Shipment Row");
             InsertText(wks, row, 2, "Part Number");
