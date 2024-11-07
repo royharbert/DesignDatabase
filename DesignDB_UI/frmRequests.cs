fd413f11f2ba5415de7748b5a2f044d70cbc3010
LDAVISON <LDAVISON@LP-9DCBLL3>
2024-11-07 10:33:54 -0800

    clone bug fixed

--- DesignDB_UI/frmRequests.cs
+++ DesignDB_UI/frmRequests.cs
@@ -1864,9 +1864,22 @@ namespace DesignDB_UI
 
         private void txtPID_TextChanged(object sender, EventArgs e)
         {
+            int saved = -1;
             if (GV.MODE == Mode.Revision)
             {
                 saveChanges();
+            }
+            if (GV.MODE == Mode.Clone)
+            {
+                saved = GlobalConfig.Connection.RequestInsert(Rm);
+                if (saved > -1)
+                {
+                    MessageBox.Show(txtPID.Text+" successfully saved");
+                }
+                else
+                {
+                    MessageBox.Show(txtPID.Text + " save unsuccessful");
+                }
             }
         }
     }   
